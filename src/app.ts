05665e1d55e8f46c254c971d33401a8d650787a2
DennisRuizB <127039219+DennisRuizB@users.noreply.github.com>
2025-03-12 17:28:29 +0100

    Añadir middleware Dennis

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express, { RequestHandler } from "express";
-import cors from "cors";
 import { startConnection } from "./database";
 import { setupSwagger } from "./swagger";
 import userRoutes from "./routes/user.routes";
@@ -7,12 +6,16 @@ import productsRoutes from "./routes/product.routes";
 import companyRoutes from "./routes/company.routes";
 import pedidosRoutes from "./routes/order.routes";
 import adminRoutes from "./routes/admin.routes";
+import { corsHandler } from "./middleware/corsHandler";
+import { loggingHandler } from "./middleware/loggingHandler";
+import { routeNotFound } from "./middleware/routeNotFound";
 
 const app = express();
 
 app.set("port", process.env.PORT || 4000);
-app.use(cors()); // Permet totes les peticions
-app.use(express.json());
+app.use(corsHandler); //Middleware para gestionar las peticiones permitidas
+app.use(loggingHandler); //Middleware para registrar las peticiones por consola
+app.use(express.json());//Middleware para convertir JSON a objetos de JS a traves de req.body
 
 app.use(express.json() as RequestHandler);
 
@@ -26,8 +29,11 @@ app.use("/api/company", companyRoutes);
 app.use("/api/orders", pedidosRoutes);
 app.use("/api/admins", adminRoutes);
 
+app.use(routeNotFound);//Middleware para informar de una ruta inexistente fuera de /users , /products ,etc.
+
 app.listen(app.get("port"), () => {
   console.log(`Server running on port ${app.get("port")}`);
+  console.log(`Swagger running at http://localhost:${app.get("port")}/api-docs/`);
 });
 
 export default app;
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -100,7 +100,7 @@ export async function getUserById(req: Request, res: Response): Promise<void> {
         const user = await userService.getUserById(id);
         res.status(200).json(user);
     } catch (error) {
-        res.status(400).json({ message: "Error getting user", error });
+        res.status(404).json({ message: "Error getting user", error });
     }
 }
 
