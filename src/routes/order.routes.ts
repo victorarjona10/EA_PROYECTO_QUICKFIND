c1d3fcc34ab5406feab75712e321c31fcc0a8912
Victor Arjona Mussach <128589446+victorarjona10@users.noreply.github.com>
2025-03-19 11:37:13 +0100

    Update order.routes.ts

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,9 +1,5 @@
 import { Router } from 'express';
-<<<<<<< HEAD
-import { postPedido, getPedidoById, deletePedidoById, updatePedidoById, /*getPedidoByUserId */} from '../controllers/order.controller';
-=======
 import { postPedido, getPedidoById, deletePedidoById, updatePedidoById, getPedidosByUserId} from '../controllers/order.controller';
->>>>>>> ordersPopulate
 
 const router = Router();
 
@@ -11,9 +7,6 @@ router.post("/", postPedido);
 router.get('/:id', getPedidoById);
 router.put('/:idUser', updatePedidoById);
 router.delete('/:idUser/:idPedido', deletePedidoById);
-<<<<<<< HEAD
-=======
 router.get("/AllOrdersByUser/:idUser", getPedidosByUserId);
->>>>>>> ordersPopulate
 
 export default router;
