9f9d6d8aff8595ae71ac8888c449ab765a8016f9
MohaLourd <mohamed.lourdjane@estudiantat.upc.edu>
2025-04-02 15:56:07 +0200

    Mejoras en admin(login para backofficce)

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -1,18 +1,18 @@
 import { IAdmin, AdminModel } from "../models/admin";
+import { Request, Response } from "express";
 
 export class AdminService {
   async postAdmin(admin: Partial<IAdmin>): Promise<IAdmin> {
     try {
-    const newAdmin = new AdminModel(admin);
-    return newAdmin.save();
-  }
-  catch (error: any) {
-    if (error.code === 11000) {
+      const newAdmin = new AdminModel(admin);
+      return newAdmin.save();
+    } catch (error: any) {
+      if (error.code === 11000) {
         throw new Error("El email ya está registrado");
+      }
+      console.log(error);
+      throw error;
     }
-    console.log(error);
-    throw error;
-}
   }
 
   async getAllAdmins(): Promise<IAdmin[]> {
@@ -24,21 +24,20 @@ export class AdminService {
   }
 
   async updateAdminById(id: string, admin: IAdmin): Promise<IAdmin | null> {
-     try{
-        if (admin.email) {
-           // Buscar si ya existe un usuario con este email
-            const existingAdmin = await AdminModel.findOne({ email: admin.email });
-        
-            // Si se encuentra un usuario con el mismo email y su ID no coincide
-            if (existingAdmin && existingAdmin._id.toString() !== id) {
-              throw new Error("El email ya está registrado");
-              }
-        }  
-    return AdminModel.findByIdAndUpdate(id, admin, { new: true });
-    }
-    catch (error: any) {
-      if (error.code === 11000) {
+    try {
+      if (admin.email) {
+        // Buscar si ya existe un usuario con este email
+        const existingAdmin = await AdminModel.findOne({ email: admin.email });
+
+        // Si se encuentra un usuario con el mismo email y su ID no coincide
+        if (existingAdmin && existingAdmin._id.toString() !== id) {
           throw new Error("El email ya está registrado");
+        }
+      }
+      return AdminModel.findByIdAndUpdate(id, admin, { new: true });
+    } catch (error: any) {
+      if (error.code === 11000) {
+        throw new Error("El email ya está registrado");
       }
       console.log(error);
       throw error;
@@ -47,5 +46,19 @@ export class AdminService {
 
   async deleteAdminById(id: string): Promise<IAdmin | null> {
     return AdminModel.findByIdAndDelete(id);
+  }
+
+  async loginAdmin(email: string, password: string): Promise<IAdmin | null> {
+    const admin = await AdminModel.findOne({ email });
+    if (!admin) {
+      throw new Error("Email o contraseña incorrectos");
+    }
+
+    // Comparación directa de contraseñas
+    if (admin.password !== password) {
+      throw new Error("Email o contraseña incorrectos");
+    }
+
+    return admin;
   }
 }
