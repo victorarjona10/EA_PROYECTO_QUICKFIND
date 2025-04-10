8d39496a4dbb052172ab49c23af02673dbae2800
MohaLourd <mohamed.lourdjane@estudiantat.upc.edu>
2025-04-10 12:58:47 +0200

    Mini moficiacion en company para que se vea en react

--- src/models/company.ts
+++ src/models/company.ts
@@ -1,5 +1,4 @@
-import {ObjectId, Schema, model} from 'mongoose';
-
+import { ObjectId, Schema, model } from "mongoose";
 
 export interface ICompany {
   _id: ObjectId;
@@ -11,25 +10,33 @@ export interface ICompany {
   phone: string;
   password: string;
   wallet: number;
+  coordenates_lat?: number;
+  coordenates_lng?: number;
   products: ObjectId[];
 }
 
-
 const companySchema = new Schema<ICompany>({
-    name: { type: String, required: true },
-    rating: { type: Number, required: true },
-    description: { type: String, required: true },
-    location: { type: String, required: true },
-    email: { type: String, required: true, unique: true, validate: {
-      validator: function(value: string) {
-        return value.includes('@');
+  name: { type: String, required: true },
+  rating: { type: Number, required: true },
+  description: { type: String, required: true },
+  location: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (value: string) {
+        return value.includes("@");
+      },
+      message: "Email must contain @",
     },
-      message: 'Email must contain @'
-    }}, 
-    phone: { type: String, required: true },
-    password: { type: String, required: true },
-    wallet: { type: Number, required: false, default: 0 },
-    products: [{ type: Schema.Types.ObjectId, ref: "Product" , required: false}],
-  });
-  
-  export const CompanyModel = model("Company", companySchema);
+  },
+  phone: { type: String, required: true },
+  password: { type: String, required: true },
+  wallet: { type: Number, required: false, default: 0 },
+  products: [{ type: Schema.Types.ObjectId, ref: "Product", required: false }],
+  coordenates_lat: { type: Number, required: false },
+  coordenates_lng: { type: Number, required: false },
+});
+
+export const CompanyModel = model("Company", companySchema);
