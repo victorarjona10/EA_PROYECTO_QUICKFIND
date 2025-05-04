313a518a0d48f06a90e9e706292c9a0ab5c04582
victorarjona10 <victor.arjona@estudiantat.upc.edu>
2025-05-04 22:18:09 +0200

    Borre lo de Docker y lo he vuelto a hacer pero compilando antes (en lo que hice del dist que teniamos mal)

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,8 @@
 import { connect, connection } from 'mongoose'
                 //mongodb://localhost:27017/proyecto
+                //mongodb://mongodb:27017/Ops
+                //mongodb+srv://victorarjona:RkJ7CyZ46HzsvC3b@quickfindeaproject.mr2tj.mongodb.net/?retryWrites=true&w=majority&appName=QuickFindEAProject
+
 const mongoURI = 'mongodb+srv://victorarjona:RkJ7CyZ46HzsvC3b@quickfindeaproject.mr2tj.mongodb.net/?retryWrites=true&w=majority&appName=QuickFindEAProject';  
 
 export async function startConnection() {
